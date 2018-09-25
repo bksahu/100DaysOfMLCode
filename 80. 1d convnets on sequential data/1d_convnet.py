eda58a475d6a62bdfb341581225334e2f5662c3d
bksahu <bablusahoo16@gmail.com>
2018-09-25 20:53:53 +0530

    Day 80 - 1d convnets on sequential data  added

---
+++ 80. 1d convnets on sequential data/1d_convnet.py
@@ -0,0 +1,61 @@
+'''
+Following is a 1d convnet implmentations on IMDB dataset.
+'''
+
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 10000
+max_len = 500
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+# create 1d convnets
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.Embedding(max_features, 128, input_length=max_len))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.MaxPooling1D(5))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(1))
+
+model.summary()
+
+model.compile(optimizer=RMSprop(lr=1e-4),
+	      loss='binary_crossentropy',
+	      metrics=['acc'])
+history = model.fit(x_train, y_train,
+		   epochs=10,
+		   batch_size=128,
+		   validation_split=0.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
